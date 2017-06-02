d96a66016c6e3cf3325bbea7d71699f4f83154e0
lemondreamtobe <897418436@qq.com>
2017-06-02 08:45:03 +0800

    solution

--- this.js
+++ this.js
@@ -1,4 +1,4 @@
-//this is magical
+//'this' is magical
 
 //getObjectType in my Lemontree.js
 function getObjectType(a) {
@@ -18,4 +18,35 @@ var div = document.getElementById("os"); //a div element
 //now test two function
 Lemontree.getObjectType(div); //""
 getType(div); //HTMLDivElement
+
+Lemontree.getObjectType(div);
+return typeArray[1].slice(0, this.length-1); 
+
+// this → object ：Lemontree
+// this.length = 0 
+// typeArray[1].slice(0, -1)  → ""
+
+getType(div); //HTMLDivElement
+return typeArray[1].slice(0, this.length-1); 
+
+// this → typeArray[1];
+// typeArray[1] = "HTMLDivElement]" 
+// this.length = 11
+// typeArray[1].slice(0, 10) → HTMLDivElement
+
+//Solution
+
+function getObjectType(a) {
+	var typeArray = Object.prototype.toString.call(a).split(" ");
+	return function(a){
+		return a.slice(0, this.length-1);
+	}(typeArray[1]);
+};
+window["Lemontree"]["getObjectType"] = getObjectType;
+Lemontree.getObjectType(div); //HTMLDivElement
+
+
+
+
+
 
